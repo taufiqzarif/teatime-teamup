3bb2ebbd9563e3115e828737c1f05c902a85601b
Taufiq Zarif <taufiqzarif90@gmail.com>
2024-02-18 19:20:47 +0800

    feat: implement remove team members from team

---
+++ src/commands/general/removeteammembers.js
@@ -0,0 +1,36 @@
+const { SlashCommandBuilder, StringSelectMenuBuilder, ActionRowBuilder } = require("discord.js");
+const Users = require("../../schema/users");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("removeteammembers")
+        .setDescription("Remove team members."),
+
+    async execute(interaction, client) {
+        await interaction.deferReply({ ephemeral: true });
+        const ownerId = interaction.user.id;
+
+        const user = await Users.findOne({ userId: ownerId });
+        if (!user) {
+            await interaction.editReply({
+                content: "You don't have any teams. To create a team, use `/createteam`.",
+                ephemeral: true,
+            });
+            return;
+        }
+
+        const actionRow = new ActionRowBuilder().setComponents(
+            new StringSelectMenuBuilder().setCustomId("remove_team_members").setPlaceholder("Select team")
+            .setOptions(user.teams.map((team) => ({
+                label: team.teamName,
+                value: team.teamName,
+            }))
+        ));
+
+        await interaction.editReply({
+            content: "Select team",
+            components: [actionRow.toJSON()],
+            ephemeral: true,
+        });
+    },
+}
