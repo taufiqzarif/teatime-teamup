7ce6f02a117528d567952fe50b86506a6e033dc8
Taufiq Zarif <taufiqzarif90@gmail.com>
2024-06-08 04:02:01 +0800

    added try-catch blocks

--- src/commands/general/deleteteam.js
+++ src/commands/general/deleteteam.js
@@ -11,35 +11,43 @@ export default {
     .setDescription("Delete a existing team"),
 
   async execute(interaction) {
-    await interaction.deferReply({ ephemeral: true });
-    const ownerId = interaction.user.id;
+    try {
+      await interaction.deferReply({ ephemeral: true });
+      const ownerId = interaction.user.id;
 
-    const user = await Users.findOne({ userId: ownerId });
-    if (!user) {
+      const user = await Users.findOne({ userId: ownerId });
+      if (!user) {
+        await interaction.editReply({
+          content:
+            "You don't have any teams. To create a team, use `/createteam`.",
+          ephemeral: true,
+        });
+        return;
+      }
+
+      const actionRow = new ActionRowBuilder().setComponents(
+        new StringSelectMenuBuilder()
+          .setCustomId("delete_team")
+          .setPlaceholder("Delete team")
+          .setOptions(
+            user.teams.map((team) => ({
+              label: team.teamName,
+              value: team.teamName,
+            }))
+          )
+      );
+
       await interaction.editReply({
-        content:
-          "You don't have any teams. To create a team, use `/createteam`.",
+        content: "Select team to delete",
+        components: [actionRow.toJSON()],
         ephemeral: true,
       });
-      return;
+    } catch (error) {
+      console.error(error);
+      await interaction.editReply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
     }
-
-    const actionRow = new ActionRowBuilder().setComponents(
-      new StringSelectMenuBuilder()
-        .setCustomId("delete_team")
-        .setPlaceholder("Delete team")
-        .setOptions(
-          user.teams.map((team) => ({
-            label: team.teamName,
-            value: team.teamName,
-          }))
-        )
-    );
-
-    await interaction.editReply({
-      content: "Select team to delete",
-      components: [actionRow.toJSON()],
-      ephemeral: true,
-    });
   },
 };
